b2901ed7a5f5bbc3c944b1e64a1759afbbacb5fc
Charlie Groves <c@sevorg.org>
2022-08-26 17:01:45 -0400

    Have linky step through by command

--- linky.py
+++ linky.py
@@ -3,30 +3,56 @@ if len(path) == 0 or path[0] != '/Users/groves/.config/kitty':
     path.insert(0, '/Users/groves/.config/kitty')
 import importer
 importer.reload_catherd_modules()
-from re import finditer
+from re import finditer, search
 from log import logger
 from nav import abspath_in_win, edit, find_shell_window
 from kittens.tui.handler import result_handler
 
 l = logger('catherd.linky')
 
-OSC_8_REGEX = '\x1b]8;(?P<params>.*?);(?P<url>.+?)(?:#(?P<fragment>.*?))?\x1b\\\\(?P<contents>(?:.|\n)*?)\x1b]8;;\x1b\\\\'
+OSC_START = '\x1b]'
+ST = '\x1b\\\\'
+OSC_8_OPEN = f'{OSC_START}8;(?P<params>.*?);(?P<url>.+?)(?:#(?P<fragment>.*?))?{ST}'
+OSC_8_CONTENT = f'(?P<contents>(?:.|\n)*?)(?:{OSC_START}8;|$)'
+OSC_133 = f'{OSC_START}133;(?P<command_type>[ACD]){ST}'
 
 def main(args) -> str:
     pass
 
-def mark(text):
+def mark_commands(text):
+    pos = 0
+    results = []
+    def add_marks(subtext):
+        marks = list(mark_command(subtext))
+        if len(marks) > 0:
+            results.append(marks)
+    for m in finditer(OSC_133, text):
+        command_type = m.group('command_type')
+        if command_type == 'C':
+            pos = m.end()
+            l.info("Found C, moving to %s", pos)
+        else:
+            l.info("Found %s, marking to %s", command_type, m.start())
+            add_marks(text[pos:m.start()])
+            pos = m.end()
+    if pos < len(text):
+        l.info("Found end at %s, marking from %s", len(text), pos)
+        add_marks(text[pos:])
+    return results
+
+def mark_command(text):
     last_line = -1
-    for m in finditer(OSC_8_REGEX, text):
-        l.info(m.groups())
-        params, url, fragment, contents = m.group('params', 'url', 'fragment', 'contents')
+    for m in finditer(OSC_8_OPEN, text):
+        params, url, fragment = m.group('params', 'url', 'fragment')
+        contents = search(OSC_8_CONTENT, text[m.end():]).group('contents')
         if url.startswith('file://'):
             fn_start = url.find('/', len('file://'))
             fn = url[fn_start:]
         elif url.startswith('delta://'):
             fn = url[len('delta://'):]
-            # Only step to added lines i.e. ones with this green text
-            if not contents.startswith('\x1b[38:5:28m '):
+            # Only step to chunk headers i.e. ones with this blue text
+            if not contents.startswith('\x1b[34m'):
+                l.info("Skipping %s", repr(contents))
                 continue
         else:
             l.info('Unknown url "%s", skipping', url)
@@ -35,9 +61,9 @@ def mark(text):
             line = int(fragment)
             # Skip over sequential lines
             sequential = last_line == line - 1
+            last_line = line
             if sequential:
                 continue
-            last_line = line
         else:
             line = 0
         yield {'line':line, 'fn':fn, 'col': 0}
@@ -49,15 +75,17 @@ def handle_result(args, data, target_window_id, boss):
     except:
         l.exception('Chunks blown!')
 
-_last_marks_attr = '_catherd.hint_edit.last_marks'
-_last_mark_idx_attr = '_catherd.hint_edit.last_mark_idx'
+_linky_attr = '_catherd.linky'
 def edit_next_link(boss):
     win = find_shell_window(boss)
-    marks = list(mark(win.as_text(as_ansi=True)))
-    if len(marks) == 0:
+    # TODO - get full history, not just visible. add_history does that, but it doesn't include links.
+    # Possible kitty bug?
+    command_links = mark_commands(win.as_text(as_ansi=True))#, add_history=True))
+    if len(command_links) == 0:
         return
-    last_marks = getattr(win, _last_marks_attr, [])
-    last_mark_idx = getattr(win, _last_mark_idx_attr, 0)
+    marks = command_links[-1]
+    last_marks, last_mark_idx = getattr(win, _linky_attr, ([], 0))
+    # TODO - continue walk if last_marks is a prefix of marks
     if last_marks == marks:
         l.info("Same marks, going to next from %s", last_mark_idx)
         mark_idx = last_mark_idx + 1
@@ -66,8 +94,8 @@ def edit_next_link(boss):
     else:
         l.info("%s new marks", len(marks))
         mark_idx = 0
+    # TODO - scroll terminal to have this visible
     result = marks[mark_idx]
     fn = abspath_in_win(win, result['fn'])
     edit(boss, fn, result['line'], result['col'])
-    setattr(win, _last_marks_attr, marks)
-    setattr(win, _last_mark_idx_attr, mark_idx)
+    setattr(win, _linky_attr, (marks, mark_idx))
