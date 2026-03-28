684c681f2859fcf0073e22fd76aa3f4589edb4cf
Charlie Groves <charliegroves@datavant.com>
2026-03-28 10:28:04 -0400

    reterm across windows

--- reterm.py
+++ reterm.py
@@ -6,6 +6,7 @@ from log import logger
 from nav import send_control_c
 from kitty.fast_data_types import KeyEvent, GLFW_FKEY_ENTER, GLFW_FKEY_UP
 from kittens.tui.handler import result_handler
+import time
 
 l = logger("catherd.reterm")  # noqa: E741
 
@@ -28,14 +29,13 @@ _id_attr = "_catherd.reterm.window_id"
 
 
 def reterm(boss):
-    tab = boss.active_tab
     win = None
-    window_id = getattr(tab, _id_attr, None)
+    window_id = getattr(boss, _id_attr, None)
     if boss.active_window.at_prompt:
         win = boss.active_window
         l.debug("At prompt %s", win)
     else:
-        for possible in tab.windows:
+        for possible in boss.all_windows:
             if possible.id == window_id:
                 win = possible
                 l.debug("Found previous win %s", win)
@@ -43,12 +43,13 @@ def reterm(boss):
         if win is None:
             l.warn("No window with known id, not at prompt, can't reterm")
             return
-    setattr(tab, _id_attr, win.id)
+    setattr(boss, _id_attr, win.id)
     if boss.active_window.id != win.id:
         l.info("Blinking focus on %s", boss.active_window)
         # Sending focus lost and gained codes to get Helix to autosave
         # https://terminalguide.namepad.de/mode/p1004/
         boss.active_window.write_to_child(b"\x1b[O\x1b[I")
+        time.sleep(0.05)
     send_control_c(win)
     encoded_keys = b"".join(win.encoded_key(key) for key in keys)
     win.write_to_child(encoded_keys)
